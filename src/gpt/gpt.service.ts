b0e3dbeda5544e065292e7566d794c2a00f2a4ba
Dario Ramirez <dariorama60@gmail.com>
2025-07-19 20:16:11 -0300

    Primera peticion a openAi

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,18 @@
 import { Body, Injectable } from '@nestjs/common';
 import { orthographyUseCases } from './use-cases/orthography.use-cases';
 import { OrthographyDto } from './dtos/orthography.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+
+    private openAi = new OpenAI({
+        apiKey: process.env.API_KEY,
+    });
+
     // Soo va a llamar casos de uso
-   
-    async orthographyCheck(orthographyDto: OrthographyDto) {
-        return await orthographyUseCases(orthographyDto);
+
+    async orthographyCheck( orthographyDto: OrthographyDto) {
+        return await orthographyUseCases( this.openAi ,{ prompt: orthographyDto.prompt});
     }
 }
